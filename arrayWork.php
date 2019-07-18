161f7fad86a81862b7489436bf74cb66f2245c31
cathysnider <catherine.snider@colorado.edu>
2019-07-18 13:31:46 -0600

    working with arrays

--- arrayWork.php
+++ arrayWork.php
@@ -3,19 +3,15 @@
 // Set some spacer constants
 define("SPACES", "&nbsp; &nbsp;");
 define("LINEBREAK", "</br />");
-define("PARAGRAPH", "<p>&nbsp;</p>");
+define("PARAGRAPH", "</br /></br />");
 
+echo "<h2>Arrays</h2>";
 
-//$linebreak = "</br />";
-//$paragraph = '<p>&nbsp;</p>';
-//$spaces = '&nbsp; &nbsp; ';
-
-// First, we create an array.
+// First, we create some arrays.
 $countries = array('China', 'Russia', 'Kiwi', 'North Korea', 'Uganda');
-$animals = array('Bandit', 'Dice', 'HammyGuy', 'Midas', 'LolaMix');
+$animals = array('dog' => 'Bandit', 'cat' => 'Muffins', 'hamster' => 'HammyGuy', 'horse' => 'LolaMix', 'anole' => 'Emilia');
 
 // Now we loop through each item and do something with it
-
 echo "<b>Using a For Loop</b>" . LINEBREAK;
 
 function myForLoop($arrayName) {
@@ -25,16 +21,48 @@ function myForLoop($arrayName) {
         echo $arrayName[$i];
         echo SPACES;
     }
+    echo PARAGRAPH;
 }
 
-myForLoop($animals);
+myForLoop($countries);
 
-echo PARAGRAPH;
+// myForLoop($animals); Doesn't work; you can't refer to associative array elements with index number.
 
-echo "<b>Using a ForEach Loop</b>" . LINEBREAK;
+echo "<b>Using a ForEach Loop; Values only</b>" . LINEBREAK;
 
-foreach ($countries as $country) {
-    print $country . SPACES;
+function myForEachLoop($arrayName)
+{
+    foreach ($arrayName as $value) {
+        print $value . SPACES;
+    }
+    echo PARAGRAPH;
 }
+
+myForEachLoop($countries);
+myForEachLoop($animals);
+
+echo "<b>Using a ForEach Loop; both Keys and Values</b>" . LINEBREAK;
+
+function myForEachLoopKV($arrayName)
+{
+    foreach ($arrayName as $key => $value) {
+        print $key . " + " . $value . LINEBREAK;
+    }
+    echo PARAGRAPH;
+}
+
+myForEachLoopKV($countries);
+myForEachLoopKV($animals);
+
+echo "<h2>Multidimensional Arrays</h2>";
+
+$myMultiArray = array(
+    'Harry' => array('year' => 1972, 'band' => 'The Cure','size' => 10,),
+    'Ron' => array('year' => 1975,'band' => 'Coldplay','size' => 8,),
+    'Hermione' => array('year' => 1969, 'band' => 'Beach Boys', 'size' => 10,)
+    );
+
+echo "<b>Using a ForEach Loop ljsdlfjs </b>" . LINEBREAK;
+
 
 ?>
